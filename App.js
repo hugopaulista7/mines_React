673cf5d7443b5afa0e422fa115f309739e7fb051
Hugo <hugoarpaulista@gmail.com>
2019-11-04 11:30:07 -0300

    iniciando projeto

---
+++ App.js
@@ -0,0 +1,34 @@
+
+import React, {Component} from 'react'
+import { StyleSheet, View, Text } from 'react-native'
+import params from './src/params'
+import Field from './src/components/Field'
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.default}>
+        <Text>Iniciando Mines!</Text>
+        <Text> Tamanho da grade: {params.getRowsAmount()}x{params.getColumnsAmount()} </Text>
+        <Field />
+        <Field opened />
+        <Field opened nearMines={1} />
+        <Field opened nearMines={2} />
+        <Field opened nearMines={3} />
+        <Field opened nearMines={6} />
+        <Field mined />
+        <Field mined opened/>
+        <Field mined opened exploded/>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  default: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+});
+
---
+++ src/components/Flag.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { View, StyleSheet } from 'react-native'
+
+export default props => {
+    
+}
